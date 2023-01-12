8d732f74fbb123cfd0facee7297c804dbb5aeae3
Wie Kiang H <wiekiang@gmail.com>
2023-01-12 21:30:15 +1000

    add Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,27 @@
 /** @format */
 
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
-    return <div>Feed</div>;
+    return (
+        <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+            <Box
+                sx={{
+                    height: { sx: "auto", md: "92vh" },
+                    borderRight: "1px solid #3d3d3d",
+                    px: { sx: 0, md: 2 },
+                }}
+            >
+                <Typography
+                    className="copyright"
+                    sx={{ mt: 1.5, color: "#fff" }}
+                >
+                    Copyright &copy; 2023. Wie Kiang Hi. All rights reserved.
+                </Typography>
+            </Box>
+        </Stack>
+    );
 };
 
 export default Feed;
